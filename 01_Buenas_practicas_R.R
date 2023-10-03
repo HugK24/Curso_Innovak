cd7cfcae73e58789b86c619baac5e93da3fee841
Hugo Barrera Chavez <hbarrera@innovak.int>
2023-10-03 16:04:39 -0600

    Primer programa

---
+++ 01_Buenas_practicas_R.R
@@ -0,0 +1,15 @@
+#### Primer programa en R y buenas precticas de programacion ####
+
+### Paquetes o librerias ###
+
+## Paqiuetes que se necesitan instalar
+
+## Paquetes ya instalados
+
+### Library ###
+
+library(dplry)
+
+### Data Upload ###
+
+read.socket()
